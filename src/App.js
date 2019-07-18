e35ffc3e85e09a9d7b0581bcacbe3444da3f5600
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-07-18 15:44:52 +0530

    bookform

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import BookContextProvider from './context/BookContext';
 import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 const App = () => {
   return (
     <div className="App">
       <BookContextProvider>
          <Navbar/> 
+         <BookList/>
+         <BookForm/>
       </BookContextProvider>
     </div>
   );
